7e5ac24f752fab294551a0c007270f4b5fc2c100
ricardovalencia1192 <ricardovalencia1192@gmail.com>
2023-06-15 18:53:32 -0500

    cambio a ruta de back

--- src/app/backend/cruds/crud.service.ts
+++ src/app/backend/cruds/crud.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CrudService {
-  private baseUrl = 'http://tu-backend.com/api'; 
+  private baseUrl = 'http://127.0.0.1:5000/'; 
 
   constructor(private http: HttpClient) { }
 
