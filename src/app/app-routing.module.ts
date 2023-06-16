7aa9dcf3c9e1d5c2175436d47b888d2d3c27a5b6
MateoAguirreO <mateoaguirre67@gmail.com>
2023-06-15 20:17:46 -0500

    arreglado by danydlpr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
+import { CompromisoComponent } from './components/compromiso/compromiso.component';
 import { ReservasPresupuestalesComponent } from './components/reservas-presupuestales/reservas-presupuestales.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'reserva', component: ReservasPresupuestalesComponent },
-=======
-import { CompromisoComponent } from './components/compromiso/compromiso.component';
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'compromiso', component: CompromisoComponent },
->>>>>>> 32c30f5c1d7335a4bf447ab9c43dd0f703c7cd58
+  { path: 'reserva', component: ReservasPresupuestalesComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< HEAD
 import { ReservasPresupuestalesComponent } from './components/reservas-presupuestales/reservas-presupuestales.component';
-=======
 import { CompromisoComponent } from './components/compromiso/compromiso.component';
->>>>>>> 32c30f5c1d7335a4bf447ab9c43dd0f703c7cd58
 
 @NgModule({
   declarations: [
