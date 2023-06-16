32c30f5c1d7335a4bf447ab9c43dd0f703c7cd58
ricardovalencia1192 <ricardovalencia1192@gmail.com>
2023-06-15 19:53:07 -0500

    Seleccionar archivo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CompromisoComponent } from './components/compromiso/compromiso.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'compromiso', component: CompromisoComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CompromisoComponent } from './components/compromiso/compromiso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CompromisoComponent
   ],
   imports: [
     BrowserModule,
