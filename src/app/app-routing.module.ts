3cba72020c160ef10bb71479f53f0fd508ce3cf8
ricardovalencia1192 <ricardovalencia1192@gmail.com>
2023-06-15 22:14:57 -0500

    fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CompromisoComponent } from './components/compromiso/compromiso.component';
+import { RevisorComponent } from './components/compromiso/revisor/revisor.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'compromiso', component: CompromisoComponent },
-  { path: 'revision', component: CompromisoComponent },
+  { path: 'revision', component: RevisorComponent },
 
 ];
 
